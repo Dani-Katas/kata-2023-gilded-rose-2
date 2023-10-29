17057b16628169f39b9d4a781b2121e0595a7bf6
Daniel Ramos <danielramosacosta1@gmail.com>
2023-10-29 18:37:37 +0100

    BackstagePasses: override age

--- src/main/java/org/example/BackstagePasses.java
+++ src/main/java/org/example/BackstagePasses.java
@@ -4,4 +4,9 @@ public class BackstagePasses extends InventoryItem {
     public BackstagePasses(Item item) {
         super(item);
     }
+
+    @Override
+    void age() {
+        super.age();
+    }
 }
