a964c8660f3dce0eb841bac8ca0f7ad93f38ea01
Daniel Ramos <danielramosacosta1@gmail.com>
2023-10-29 17:44:37 +0100

    import gilded rose code from emily's repo

--- src/main/java/org/example/ExampleCollaborator.java
+++
@@ -1,7 +0,0 @@
-package org.example;
-
-public final class ExampleCollaborator {
-  public int sum(int a, int b) {
-    return a + b;
-  }
-}
---
+++ src/test/java/org/example/GildedRoseTest.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+class GildedRoseTest {
+    @Test
+    void test() {
+        assertThat(true).isTrue();
+    }
+}
