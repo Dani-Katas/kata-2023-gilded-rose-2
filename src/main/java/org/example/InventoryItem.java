965a306dc7e4e83f1686e85d581e397cbce28f9c
Daniel Ramos <danielramosacosta1@gmail.com>
2023-10-29 19:17:20 +0100

    remove unnecesary switch

--- src/main/java/org/example/InventoryItem.java
+++ src/main/java/org/example/InventoryItem.java
@@ -23,36 +23,10 @@ public class InventoryItem {
     }
 
     void age() {
-        switch ("any other") {
-            case AGED_BRIE_NAME:
-                increaseQuality();
-                decreaseSellIn();
-                if (hasExpired()) {
-                    increaseQuality();
-                }
-                break;
-            case BACKSTAGE_PASSES_NAME:
-                increaseQuality();
-                if (sellInIsLessThan(11)) {
-                    increaseQuality();
-                }
-                if (sellInIsLessThan(6)) {
-                    increaseQuality();
-                }
-                decreaseSellIn();
-                if (hasExpired()) {
-                    dropQualityToZero();
-                }
-                break;
-            case SULFURAS_NAME:
-                break;
-            default:
-                decreaseQuality();
-                decreaseSellIn();
-                if (hasExpired()) {
-                    decreaseQuality();
-                }
-                break;
+        decreaseQuality();
+        decreaseSellIn();
+        if (hasExpired()) {
+            decreaseQuality();
         }
     }
 
