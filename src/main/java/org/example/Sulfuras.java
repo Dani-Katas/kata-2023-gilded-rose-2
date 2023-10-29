df488ae5dc6f604d8d7856a665b5e78696d2dda4
Daniel Ramos <danielramosacosta1@gmail.com>
2023-10-29 18:37:19 +0100

    Sulfuras: simplify

--- src/main/java/org/example/Sulfuras.java
+++ src/main/java/org/example/Sulfuras.java
@@ -6,37 +6,5 @@ public class Sulfuras extends InventoryItem {
     }
 
     @Override
-    void age() {
-        switch (SULFURAS_NAME) {
-            case AGED_BRIE_NAME:
-                increaseQuality();
-                decreaseSellIn();
-                if (hasExpired()) {
-                    increaseQuality();
-                }
-                break;
-            case BACKSTAGE_PASSES_NAME:
-                increaseQuality();
-                if (sellInIsLessThan(11)) {
-                    increaseQuality();
-                }
-                if (sellInIsLessThan(6)) {
-                    increaseQuality();
-                }
-                decreaseSellIn();
-                if (hasExpired()) {
-                    dropQualityToZero();
-                }
-                break;
-            case SULFURAS_NAME:
-                break;
-            default:
-                decreaseQuality();
-                decreaseSellIn();
-                if (hasExpired()) {
-                    decreaseQuality();
-                }
-                break;
-        }
-    }
+    void age() {}
 }
