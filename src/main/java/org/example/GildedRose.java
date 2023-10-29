5a3fdc5717cc69d6606ef0edc7e144b450495341
Daniel Ramos <danielramosacosta1@gmail.com>
2023-10-29 18:12:11 +0100

    convert age in instance method

--- src/main/java/org/example/GildedRose.java
+++ src/main/java/org/example/GildedRose.java
@@ -9,58 +9,8 @@ class GildedRose {
 
     public void updateQuality() {
         for (Item item : items) {
-            age(new InventoryItem(item));
+            new InventoryItem(item).age();
         }
     }
 
-    private static void age(InventoryItem inventoryItem) {
-        if (!inventoryItem.item().name.equals("Aged Brie")
-                && !inventoryItem.item().name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-            if (inventoryItem.item().quality > 0) {
-                if (!inventoryItem.item().name.equals("Sulfuras, Hand of Ragnaros")) {
-                    inventoryItem.item().quality = inventoryItem.item().quality - 1;
-                }
-            }
-        } else {
-            if (inventoryItem.item().quality < 50) {
-                inventoryItem.item().quality = inventoryItem.item().quality + 1;
-
-                if (inventoryItem.item().name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                    if (inventoryItem.item().sellIn < 11) {
-                        if (inventoryItem.item().quality < 50) {
-                            inventoryItem.item().quality = inventoryItem.item().quality + 1;
-                        }
-                    }
-
-                    if (inventoryItem.item().sellIn < 6) {
-                        if (inventoryItem.item().quality < 50) {
-                            inventoryItem.item().quality = inventoryItem.item().quality + 1;
-                        }
-                    }
-                }
-            }
-        }
-
-        if (!inventoryItem.item().name.equals("Sulfuras, Hand of Ragnaros")) {
-            inventoryItem.item().sellIn = inventoryItem.item().sellIn - 1;
-        }
-
-        if (inventoryItem.item().sellIn < 0) {
-            if (!inventoryItem.item().name.equals("Aged Brie")) {
-                if (!inventoryItem.item().name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                    if (inventoryItem.item().quality > 0) {
-                        if (!inventoryItem.item().name.equals("Sulfuras, Hand of Ragnaros")) {
-                            inventoryItem.item().quality = inventoryItem.item().quality - 1;
-                        }
-                    }
-                } else {
-                    inventoryItem.item().quality = 0;
-                }
-            } else {
-                if (inventoryItem.item().quality < 50) {
-                    inventoryItem.item().quality = inventoryItem.item().quality + 1;
-                }
-            }
-        }
-    }
 }
