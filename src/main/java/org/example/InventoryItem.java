149feb99a10ea0ceb13a828fa20526a6b571ab04
Daniel Ramos <danielramosacosta1@gmail.com>
2023-10-29 18:17:01 +0100

    second if: split sulfuras if in two

--- src/main/java/org/example/InventoryItem.java
+++ src/main/java/org/example/InventoryItem.java
@@ -50,9 +50,10 @@ public final class InventoryItem {
                 if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
                     item.quality = 0;
                 } else {
-                    if (!item.name.equals("Sulfuras, Hand of Ragnaros") && item.quality > 0) {
-                        item.quality = item.quality - 1;
-                    }
+                    if (!item.name.equals("Sulfuras, Hand of Ragnaros"))
+                        if (item.quality > 0) {
+                            item.quality = item.quality - 1;
+                        }
                 }
             }
         }
