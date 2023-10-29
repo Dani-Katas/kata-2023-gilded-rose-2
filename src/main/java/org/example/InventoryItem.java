b6748093459c0df991450b9cec65cbb2befe9e6b
Daniel Ramos <danielramosacosta1@gmail.com>
2023-10-29 19:08:27 +0100

    extract method decreaseQuality

--- src/main/java/org/example/InventoryItem.java
+++ src/main/java/org/example/InventoryItem.java
@@ -38,17 +38,19 @@ public final class InventoryItem {
         } else if (name.equals("Sulfuras, Hand of Ragnaros")) {
 
         } else {
-            if (item.quality > 0) {
-                item.quality = item.quality - 1;
-            }
+            decreaseQuality();
 
             item.sellIn = item.sellIn - 1;
 
             if (item.sellIn < 0) {
-                if (item.quality > 0) {
-                    item.quality = item.quality - 1;
-                }
+                decreaseQuality();
             }
+        }
+    }
+
+    private void decreaseQuality() {
+        if (item.quality > 0) {
+            item.quality = item.quality - 1;
         }
     }
 
