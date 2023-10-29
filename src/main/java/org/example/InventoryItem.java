cd0581ee00bb1c34c579920c0ca4458b5956509b
Daniel Ramos <danielramosacosta1@gmail.com>
2023-10-29 18:19:40 +0100

    first if: simplify increase quality if

--- src/main/java/org/example/InventoryItem.java
+++ src/main/java/org/example/InventoryItem.java
@@ -17,17 +17,17 @@ public final class InventoryItem {
         } else if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
             if (item.quality < 50) {
                 item.quality = item.quality + 1;
+            }
 
-                if (item.sellIn < 11) {
-                    if (item.quality < 50) {
-                        item.quality = item.quality + 1;
-                    }
+            if (item.sellIn < 11) {
+                if (item.quality < 50) {
+                    item.quality = item.quality + 1;
                 }
+            }
 
-                if (item.sellIn < 6) {
-                    if (item.quality < 50) {
-                        item.quality = item.quality + 1;
-                    }
+            if (item.sellIn < 6) {
+                if (item.quality < 50) {
+                    item.quality = item.quality + 1;
                 }
             }
         } else if (item.name.equals("Sulfuras, Hand of Ragnaros")) {
