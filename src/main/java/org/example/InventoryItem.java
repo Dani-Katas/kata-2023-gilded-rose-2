f4e0e67440468e1749a28425768e859a8cc85aee
Daniel Ramos <danielramosacosta1@gmail.com>
2023-10-29 18:11:20 +0100

    convert record InventoryItem to Class

--- src/main/java/org/example/InventoryItem.java
+++ src/main/java/org/example/InventoryItem.java
@@ -1,4 +1,35 @@
 package org.example;
 
-public record InventoryItem(Item item) {
+import java.util.Objects;
+
+public final class InventoryItem {
+    private final Item item;
+
+    public InventoryItem(Item item) {
+        this.item = item;
+    }
+
+    public Item item() {
+        return item;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this) return true;
+        if (obj == null || obj.getClass() != this.getClass()) return false;
+        var that = (InventoryItem) obj;
+        return Objects.equals(this.item, that.item);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(item);
+    }
+
+    @Override
+    public String toString() {
+        return "InventoryItem[" +
+                "item=" + item + ']';
+    }
+
 }
